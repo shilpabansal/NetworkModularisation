67048c927d533bdec046e4477fee67163f1667a3
Shilpa Bansal <shilpa.bansal@walmartlabs.com>
2021-01-24 19:33:19 +0530

    Added shared factory method to compare the failure and success response of the test cases

--- NetworkModularizationTests/Feed Cache/LoadFeedFromCacheTests.swift
+++ NetworkModularizationTests/Feed Cache/LoadFeedFromCacheTests.swift
@@ -31,44 +31,16 @@ class LoadFeedFromCacheTests: XCTestCase {
     func test_load_feedRequestError() {
         let (store, feedLoader) = makeSUT()
         let retrievalError = anyNSError()
-        
-        var receivedError: Error?
-        
-        let exp = expectation(description: "Wait for api")
-        feedLoader.loadFeeds({result in
-            switch result {
-            case .failure(let error):
-                receivedError = error
-            default:
-                XCTFail("Expected failure, received \(result)")
-            }
-            exp.fulfill()
+        expect(feedLoader, toCompleteWith: .failure(retrievalError), when: {
+            store.completeRetrieval(with: retrievalError)
         })
-        
-        store.completeRetrieval(with: retrievalError)
-        wait(for: [exp], timeout: 1.0)
-        XCTAssertEqual(receivedError as NSError?, retrievalError)
     }
     
     func test_load_deliversNoImagesOnEmptyCache() {
         let (store, feedLoader) = makeSUT()
-        var receivedImages: [FeedImage]?
-        
-        let exp = expectation(description: "Wait for api")
-        feedLoader.loadFeeds({result in
-            switch result {
-            case .success(let images):
-                receivedImages = images
-            default:
-                XCTFail("Expected success, received \(result)")
-            }
-            
-            exp.fulfill()
+        expect(feedLoader, toCompleteWith: .success([]), when: {
+            store.completeRetrievalSuccessfully()
         })
-        
-        store.completeRetrievalSuccessfully()
-        wait(for: [exp], timeout: 1.0)
-        XCTAssertEqual(receivedImages, [])
     }
     
     //MARK: - Helpers
@@ -85,4 +57,25 @@ class LoadFeedFromCacheTests: XCTestCase {
     private func anyNSError() -> NSError {
         return NSError(domain: "Test Error", code: 1)
     }
+    
+    private func expect(_ sut: LocalFeedLoader,
+                        toCompleteWith expectedResult: LocalFeedLoader.LoadResult,
+                        when action: (() -> Void),
+                        file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for api")
+        sut.loadFeeds({receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedImages), .success(expectedImages)):
+               XCTAssertEqual(receivedImages, expectedImages)
+            case let (.failure(receivedError as NSError?), .failure(expectedError as NSError?)):
+               XCTAssertEqual(receivedError, expectedError)
+            default:
+                XCTFail("Expected \(expectedResult), received \(receivedResult)")
+            }
+            exp.fulfill()
+        })
+        action()
+        wait(for: [exp], timeout: 1.0)
+    }
 }
+
