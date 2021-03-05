5e821f79d98f43907425d7a1e5045461c2994fa8
Shilpa Bansal <shilpa.bansal@walmartlabs.com>
2021-03-05 09:07:39 +0530

    Since Swift5 provides Result type which has Result<Success, Failure> structure, similar to the requirement we have for LoadFeedResult. Converted LoadFeedResult to typealias of Result

--- NetworkModularization/Feed Feature/FeedLoader.swift
+++ NetworkModularization/Feed Feature/FeedLoader.swift
@@ -6,10 +6,7 @@
 //
 
 import Foundation
-public enum LoadFeedResult {
-    case success([FeedImage])
-    case failure(Error)
-}
+public typealias LoadFeedResult = Result<[FeedImage], Error>
 
 public protocol FeedLoader {
     func getFeeds(completion: @escaping ((LoadFeedResult) -> Void))
