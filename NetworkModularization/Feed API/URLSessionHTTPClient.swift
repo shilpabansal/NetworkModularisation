60ebdd1d4fc3aab73bf64b41a4cb20d9bdc57ec1
Shilpa Bansal <shilpa.bansal@walmartlabs.com>
2021-03-06 16:10:19 +0530

    Since Result initialiser block returns success and in case of failure handles catch block. Moved conditions to throw or return the data

--- NetworkModularization/Feed API/URLSessionHTTPClient.swift
+++ NetworkModularization/Feed API/URLSessionHTTPClient.swift
@@ -21,15 +21,17 @@ public class URLSessionHTTPClient: HTTPClient {
     
     public func loadFeeds(url: URL, completion: @escaping ((HTTPClient.Result) -> Void)) {
         session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                completion(.failure(error))
-            }
-            else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success((data, response)))
-            }
-            else {
-                completion(.failure(UnexpectedValueRepresentation()))
-            }
+            completion(Result {
+                if let error = error {
+                    throw error
+                }
+                if let data = data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+               }
+                else {
+                    throw UnexpectedValueRepresentation()
+                }
+            })
         }.resume()
     }
 }
