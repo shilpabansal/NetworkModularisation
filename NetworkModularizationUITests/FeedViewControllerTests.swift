b92aa4c3351c5bc49062db6f2f2f27573ae4c5dd
Shilpa Bansal <shilpa.bansal@walmartlabs.com>
2021-03-11 19:54:08 +0530

    Added test case to check if feeds are loading on load call

--- NetworkModularizationUITests/FeedViewControllerTests.swift
+++ NetworkModularizationUITests/FeedViewControllerTests.swift
@@ -5,14 +5,22 @@
 //  Created by Shilpa Bansal on 09/03/21.
 //
 
-import Foundation
 import XCTest
+import UIKit
 
-final class FeedViewController {
-    let loader: FeedViewControllerTests.LoaderSpy
+final class FeedViewController: UIViewController {
+    var loader: FeedViewControllerTests.LoaderSpy? = nil
     
-    init(loader: FeedViewControllerTests.LoaderSpy) {
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init()
+        
         self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loader?.load()
     }
 }
 
@@ -25,7 +33,18 @@ class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
+    func test_load_loadsFeed() {
+        let loader = LoaderSpy()
+        _ = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
     struct LoaderSpy {
-        let loadCallCount = 0
+        private(set) var loadCallCount = 0
+        
+        mutating func load() {
+            loadCallCount += 1
+        }
     }
 }
