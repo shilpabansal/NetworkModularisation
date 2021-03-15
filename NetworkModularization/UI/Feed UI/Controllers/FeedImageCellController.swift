0bef42a2746bc89dc213c819e089cb5539f9f4dd
Shilpa Bansal <shilpa.bansal@walmartlabs.com>
2021-03-15 14:59:06 +0530

    Moved FeedImageCellController to its own file

---
+++ NetworkModularization/UI/Feed UI/Controllers/FeedImageCellController.swift
@@ -0,0 +1,52 @@
+//
+//  FeedImageCellController.swift
+//  NetworkModularization
+//
+//  Created by Shilpa Bansal on 15/03/21.
+//
+
+import UIKit
+final class FeedImageCellController {
+    var task: FeedImageDataLoaderTask? = nil
+    let model: FeedImage
+    let imageLoader: FeedImageDataLoader?
+    
+    init(model: FeedImage, imageLoader: FeedImageDataLoader?) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    func view() -> UITableViewCell {
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (model.location == nil)
+        cell.locationLabel.text = model.location
+        cell.descriptionLabel.text = model.description
+        cell.feedImageView.image = nil
+        cell.feedImageRetryButton.isHidden = true
+        cell.feedImageContainer.startShimmering()
+
+        let loadImage = { [weak self, weak cell] in
+            guard let self = self else { return }
+
+            self.task = self.imageLoader?.loadImageData(from: self.model.url) { [weak cell] result in
+                let data = try? result.get()
+                let image = data.map(UIImage.init) ?? nil
+                cell?.feedImageView.image = image
+                cell?.feedImageRetryButton.isHidden = (image != nil)
+                cell?.feedImageContainer.stopShimmering()
+            }
+        }
+
+        cell.onRetry = loadImage
+        loadImage()
+        return cell
+    }
+    
+    func preload() {
+        task = self.imageLoader?.loadImageData(from: self.model.url) { _ in }
+    }
+    
+    deinit {
+        task?.cancel()
+    }
+}
