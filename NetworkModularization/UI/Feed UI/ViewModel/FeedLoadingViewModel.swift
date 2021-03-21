bef69625ad6add75df810ffadc5a624a1152f388
Shilpa Bansal <shilpa.bansal@walmartlabs.com>
2021-03-21 19:57:51 +0530

    Moved FeedViewModel and FeedImageViewModel to their own classes

---
+++ NetworkModularization/UI/Feed UI/ViewModel/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  NetworkModularization
+//
+//  Created by Shilpa Bansal on 21/03/21.
+//
+
+struct FeedLoadingViewModel {
+    var isLoading: Bool
+}
---
+++ NetworkModularization/UI/Feed UI/ViewModel/FeedViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedViewModel.swift
+//  NetworkModularization
+//
+//  Created by Shilpa Bansal on 21/03/21.
+//
+
+struct FeedViewModel {
+    var feeds: [FeedImage]
+}
